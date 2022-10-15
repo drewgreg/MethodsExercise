6ba645b56b584802b5c54f0711664c6535d142be
Drew Gregory <drewgregory@otmail.com>
2022-10-15 16:19:57 -0700

    begin user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace MethodsExercise
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($" {userName}, what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{userName}, what is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"{userName}, what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
